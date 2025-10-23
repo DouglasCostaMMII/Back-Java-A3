b42e160ae2946696113ce5caf4a19beb855771cb
Douglas Costa <117300520+DouglasCostaMMII@users.noreply.github.com>
2025-10-23 20:05:17 -0300

    iniciei o projeto

---
+++ Projeto-backend-A3/src/main/java/com/mycompany/projeto/backend/a3/ProjetoBackendA3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto.backend.a3;
+
+/**
+ *
+ * @author douglas
+ */
+public class ProjetoBackendA3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
